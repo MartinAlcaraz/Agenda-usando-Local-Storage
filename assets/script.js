7adc21d5b558a5b54b178453ada69468b4a5dcde
MartinAlcaraz <96241107+MartinAlcaraz@users.noreply.github.com>
2022-03-26 09:16:16 -0300

    retoque responsive

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,20 @@
 import { agregarNuevaTarjeta } from "./agregar-tarjeta-nueva.js";
 import { displayTarjetas } from "./displayTarjetas.js";
 
-displayTarjetas();
+if ( localStorage.getItem("agenda-web") != null){
+    displayTarjetas();    
+}
 
 const botonAgregar = document.querySelector("[data-form-btn]");
 
 const input = document.querySelector("[data-form-input]");
 
-input.addEventListener("keydown", ocultarAlert => {
+input.addEventListener("keydown", () => {
     const alert= document.querySelector("[data-alert]");
     alert.classList.remove("alert-show");
 });
+
+
 
 botonAgregar.addEventListener("click", agregarNuevaTarjeta);
 
